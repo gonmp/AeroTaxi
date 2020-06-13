8377cda2a18fc40e34a8260883f30adf8aba468f
Gonzalo Perelstein <36910726+gonmp@users.noreply.github.com>
2020-06-12 22:17:59 -0300

    Modificado la clase ArchivoDatos

--- src/control/ArchivoDatos.java
+++ src/control/ArchivoDatos.java
@@ -2,4 +2,6 @@ package control;
 
 public class ArchivoDatos {
     public final static String archivoUsuarios = "Usuarios.json";
+    public final static String archivoAviones = "Aviones.json";
+    public final static String archivoVuelos = "Vuelos.json";
 }
